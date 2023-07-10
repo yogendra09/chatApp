ad6f63ce4f13a3a0362dc935c1faf4b443fe2336
yogendra09 <yogendrayadav168@gmail.com>
2023-07-11 00:09:59 +0530

    completed

--- socketapi.js
+++ socketapi.js
@@ -4,23 +4,33 @@ const socketapi = {
 };
 
 
- var activeUsers = [];
+ var activeUsersId = [];
+ var activeUsersName = [];
 
 // Add your socket.io logic here!
 io.on( "connection", function( socket ) {
 
     console.log( "A user connected" );
-    activeUsers.push(socket.id);
-    io.emit("userPushed",activeUsers.length);
+   
+   var naam;
 
     socket.on("disconnect",function(){
         console.log("user disconnected");
-        activeUsers.splice(activeUsers.indexOf(socket.id),1);
+        activeUsersId.splice(activeUsersId.indexOf(socket.id),1);
+        activeUsersId.splice(activeUsersName.indexOf(socket.id),1);
     })
+
+    socket.on("setName",function(data){
+        activeUsersId.push(socket.id);
+        activeUsersName.push(socket.name);
+       io.emit("activeUsers",activeUsersId.length);
+       naam = data.name;
+    })
+
 
   
     socket.on('chat message',function(msg){
-        io.emit('chat message', msg);
+        io.emit('chat message', msg , naam );
     })
 
      
